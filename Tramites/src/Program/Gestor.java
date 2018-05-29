16a7e454c5f5f2723211ee3a4e6f8a8b82ae1b72
gopovesteban <36285981+gopovesteban@users.noreply.github.com>
2018-05-29 12:52:28 -0600

    Itento fallido coneccion sql

--- Tramites/src/Program/Gestor.java
+++ Tramites/src/Program/Gestor.java
@@ -1,5 +1,42 @@
 package Program;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Gestor {
 
+	public static void prueba() {
+	String dbURL = "jdbc:sqlserver:SJOLTP214A\\SQLEXPRESS,144;Initial Catalog=DB_PROCESOS;Integrated Security=True" ;
+{
+	 
+		try (Connection conn = DriverManager.getConnection(dbURL)) {
+		     
+			String sql = "INSERT INTO usuario (nombre, apellido, tipo, firma) VALUES (?, ?, ?, ?)";
+			 
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, "Esteban");
+			statement.setString(2, "Gonzalez");
+			statement.setString(3, "cliente");
+			statement.setString(4, "vesgon");
+			 
+			int rowsInserted = statement.executeUpdate();
+			if (rowsInserted > 0) {
+			    System.out.println("A new user was inserted successfully!");
+			}
+		     
+		} catch (SQLException ex) {
+		    ex.printStackTrace();
+		}
+	
+	
+	
 }
+	}
+	
+	
+	
+}
+
+
+
--- Tramites/src/Program/UI.java
+++ Tramites/src/Program/UI.java
@@ -5,6 +5,8 @@ public class UI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		Gestor gestor = new Gestor();
+		gestor.prueba();
 	}
 
 }
